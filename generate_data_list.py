ffa47ff65a9dd73eca082bd59d97c996da67384d
KomoriDev <mute231010@gmail.com>
2025-01-17 09:54:02 +0800

    :bento: 整理资源

--- generate_data_list.py
+++ generate_data_list.py
@@ -3,40 +3,12 @@ import hashlib
 import zipfile
 from pathlib import Path
 
+file_list = []
+
 data_path = Path(__file__).parent / "data"
-font_path = Path(__file__).parent / "font"
-img_path = Path(__file__).parent / "image"
-template_path = Path(__file__).parent / "templates"
-
-file_list = [{
-    'path': f"{file.parent.name}/{file.name}",
-    'hash': hashlib.md5(file.read_bytes()).hexdigest(),
-    'lock': True
-} for file in font_path.iterdir()]
-
 this_path = str(Path().absolute())
 
 for file in data_path.rglob("*"):
-    if not file.is_file():
-        continue
-
-    file_list.append({
-        'path': str(file.relative_to(this_path)).replace("\\", "/"),
-        'hash': hashlib.md5(file.read_bytes()).hexdigest(),
-        'lock': True
-    })
-
-for file in img_path.rglob("*"):
-    if not file.is_file():
-        continue
-
-    file_list.append({
-        'path': str(file.relative_to(this_path)).replace("\\", "/"),
-        'hash': hashlib.md5(file.read_bytes()).hexdigest(),
-        'lock': True
-    })
-
-for file in template_path.rglob("*"):
     if not file.is_file():
         continue
 
@@ -52,13 +24,7 @@ with open('abscension_data_list.json', 'w', encoding='utf-8') as f:
 resources_zip_path = Path() / 'resources.zip'
 resources_zip = zipfile.ZipFile(resources_zip_path, 'w', zipfile.ZIP_DEFLATED)
 
-for file in Path('font').iterdir():
-    resources_zip.write(file)
 for file in Path('data').rglob('*'):
-    resources_zip.write(file)
-for file in Path('image').rglob('*'):
-    resources_zip.write(file)
-for file in Path('templates').rglob('*'):
-    resources_zip.write(file)
+    resources_zip.write(file, file.relative_to('data'))
 
 resources_zip.close()
